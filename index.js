ddf415f403edae2f09819ef05c0608289c16af77
Bryt Xin Li <bryt.xin.li@gmail.com>
2019-02-13 10:50:11 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const scrape = require('website-scraper')
+const options = {
+    urls: ['https://alexanderandjamessofas.com/'],
+    directory: './static/',
+}
+
+// with async/await
+const result = await scrape(options)
+
+// with promise
+//scrape(options).then(result => {})
+
+// or with callback
+//scrape(options, (error, result) => {})
